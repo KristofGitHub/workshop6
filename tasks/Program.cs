aa27eb21dc6f6ce1174274c005613d0b41badba0
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-26 18:49:53 +0500

    3rd commit: third task

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -20,21 +20,47 @@
 // }
 
 // Ввывести числа Фибоначчи без использования рекурсии.
-int f_prefinal = 0;
-int f_final = 1;
-Console.Write($"{f_prefinal}, ");
-Console.Write($"{f_final}, ");
+// int f_prefinal = 0;
+// int f_final = 1;
+// Console.Write($"{f_prefinal}, ");
+// Console.Write($"{f_final}, ");
 
-for(int i = 0; i < 40; i++) 
+// for(int i = 0; i < 40; i++) 
+// {
+//     int f_next = GetFibonachiNextTerm(f_prefinal, f_final);
+//     Console.Write($"{f_next}, ");
+//     f_prefinal = f_final;
+//     f_final = f_next;
+// }
+
+// int GetFibonachiNextTerm(int f_prefinal, int f_final)
+// {
+//     int f_next;
+//     return f_next = f_prefinal + f_final;
+// }
+
+// Развернуть массив.
+int[] array = CreateRandomArray(9, -9, 9);
+PrintArray(array);
+int[] reverseArray = ArrayReverse(array);
+PrintArray(reverseArray);
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
-    int f_next = GetFibonachiNextTerm(f_prefinal, f_final);
-    Console.Write($"{f_next}, ");
-    f_prefinal = f_final;
-    f_final = f_next;
+    int[] newArray = new int[size];
+    for(int index = 0; index < size; index++){newArray[index] = new Random().Next(minValue, maxValue + 1);}
+    return newArray;
 }
 
-int GetFibonachiNextTerm(int f_prefinal, int f_final)
+void PrintArray(int[] arrayExample)
 {
-    int f_next;
-    return f_next = f_prefinal + f_final;
+    int size = arrayExample.Length;
+    for(int index = 0; index < size; index++){Console.Write(arrayExample[index] + " ");}
+    Console.WriteLine();
+}
+
+int[] ArrayReverse(int[] array)
+{
+    for(int i = 0; i < array.Length/2; i++) (array[i], array[(array.Length - 1) - i]) = (array[(array.Length - 1) - i], array[i]);
+    return array;
 }
