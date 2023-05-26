06df07ad4b3e2f1bde1cf2ee68347c337f27d0e2
Vasiliev Sergei <sergei.binp@gmail.com>
2023-05-26 17:06:59 +0500

    1st commit:: first task

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, возможен ли треугольник со сторонами такой длины.
+// Главное правило: каждая сторона должна быть строго меньше суммы длин двух других сторон.
+// int a = GetUserInput("a");
+// int b = GetUserInput("b");
+// int c = GetUserInput("c");
+// if (CheckTriangleExist(a, b, c)) Console.WriteLine("Треугольник существует.");
+// else Console.WriteLine("Треугольник не существует");
+
+// bool CheckTriangleExist(int a, int b, int c)
+// {
+//     if ((a < b + c) && (b < a + c) && (c < a + b)) return true;
+//     else return false;
+// }
+
+// int GetUserInput(string name)
+// {
+//     Console.Write($"Введите длину стороны {name} = ");
+//     int userInput = Convert.ToInt32(Console.ReadLine());
+//     return userInput;
+// }
+
+// Ввывести числа Фибоначчи без использования рекурсии.
